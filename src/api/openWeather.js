e1c9c907a794e50072ef77d3f9f131688784e753
Carl Centola <carl.centola@gmail.com>
2021-11-29 10:35:58 -0700

    added PROD keys

--- src/api/openWeather.js
+++ src/api/openWeather.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-const key = process.env.REACT_APP_API_KEY;
+let key;
+
+if (process.env.NODE_ENV !== "production") {
+  key = process.env.REACT_APP_API_KEY;
+} else {
+  key = process.env.API_KEY;
+}
 
 export default axios.create({
   baseURL: "https://api.openweathermap.org/data/2.5",
