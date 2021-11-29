9036fe85057d535922b1477e6fdf0c5f2d09fa94
Carl Centola <carl.centola@gmail.com>
2021-11-29 09:30:46 -0700

    updated to secure request

--- src/api/openWeather.js
+++ src/api/openWeather.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const key = process.env.REACT_APP_API_KEY;
 
 export default axios.create({
-  baseURL: 'http://api.openweathermap.org/data/2.5',
+  baseURL: "https://api.openweathermap.org/data/2.5",
   params: {
     appid: key,
-    units: 'imperial',
+    units: "imperial",
   },
 });
